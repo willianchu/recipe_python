469a1b8ea2d492b4b570ef08c6504f741130525f
willianchu <willianchu@hotmail.com>
2022-05-15 21:49:07 -0700

    create project and recipe blog app

---
+++ MasterChief/MasterChief/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MasterChief project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MasterChief.settings')
+
+application = get_wsgi_application()
