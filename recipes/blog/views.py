b3e2418cf0bcf84b0dbab75168bbeb14ad72bbc2
willianchu <willianchu@hotmail.com>
2022-05-16 13:21:11 -0700

    crud operations

--- recipes/blog/views.py
+++ recipes/blog/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BlogForm
+from .models import Blog
 
-# Create your views here.
+# Add Blog
+
+def create_blog(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('search/')
+            except:
+                pass
+    else:
+        form = BlogForm()
+    return render(request, 'create.html', {'form':form})
+
+# Update Blog
+
+def update_blog(request,pk):
+    blogs = Blog.objects.get(id=pk)
+    form = BlogForm(instance=blogs)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blogs)
+        if form.is_valid():
+            form.save()
+            return redirect('/search')
+
+    context = {
+        'blogs': blogs,
+        'form': form,
+    }
+    return render(request,'update.html',context)
+    
+# Delete Blog
+
+def delete_blog(request, pk):
+    blogs = Blog.objects.get(id=pk)
+
+    if request.method == 'POST':
+        blogs.delete()
+        return redirect('/search')
+
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'remove.html', context)
--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'recipes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'recipe',  
+        'USER':'hermione',  
+        'PASSWORD':'',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
     }
 }
 
